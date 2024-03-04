d265d24cc93860f177541984e7867c0b11d3c9ce
Enzo Makked <zakuu@air-de-enzo.home>
2024-03-04 10:57:59 +0100

    firebase connection added

---
+++ src/core/firebase/firebase.js
@@ -0,0 +1,19 @@
+import { getAuth } from "firebase/auth";
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from "firebase/firestore"
+import { getStorage } from "firebase/storage"
+import 'firebase/auth'
+
+const app =  initializeApp({
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID
+  });
+
+export const auth = getAuth(app);
+export const db = getFirestore(app)
+export const storage = getStorage(app)
+export default app
