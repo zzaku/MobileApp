3cc78b216335c2778238c5357c88ca12586b6f9d
Moussa Bamba <moussabulls1@gmail.com>
2024-03-08 14:20:05 +0100

    fix conflict(src/vues/Auth.jsx)

--- src/vues/Auth.jsx
+++ src/vues/Auth.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { SafeAreaView, StatusBar, View, Text, Button } from "react-native";
 import { Login } from "../components/auth/Login";
 import { Register } from "../components/auth/Register";
@@ -7,21 +6,13 @@ import { AuthProvider } from "../core/context/firebaseContext";
 
 export function Auth({navigation}) {
   const [login, setLogin] = useState(true);
-=======
-import Register from "../components/auth/Register";
-import { SafeAreaView, StatusBar, TouchableOpacity, Text } from 'react-native';
->>>>>>> acb34d1 (home page & add project)
 
   return (
     <SafeAreaView>
-<<<<<<< HEAD
       {login ? <Login navigation={navigation} /> : <Register navigation={navigation}/>}
       <Button onPress={() => setLogin(true)} title="Login" />
       <Button onPress={() => setLogin(false)} title="register" />
-=======
       <Text>Auth</Text>
-      <StatusBar style="Project" />
->>>>>>> acb34d1 (home page & add project)
     </SafeAreaView>
   );
 }
